6f4e123966defab8444a7134726a9f235392d2cc
Dita Nurhalimah <ditanurhalimah@gmail.com>
2023-05-08 15:19:41 +0700

    chore: implement eslint on server.ts

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,14 +1,14 @@
-import 'module-alias/register';
-import express, {Application, Request, Response} from 'express';
+import 'module-alias/register'
+import express, { type Application, type Request, type Response } from 'express'
 
-const app: Application = express();
+const app: Application = express()
 
-const PORT: number = 3001;
+const PORT: number = 3001
 
 app.use('/', (req: Request, res: Response): void => {
-    res.send('Hello World');
+  res.send('Hello World')
 })
 
 app.listen(PORT, (): void => {
-    console.log(`SERVER IS UP ON PORT TEST: ${PORT}`);
+  console.log(`SERVER IS UP ON PORT TEST: ${PORT}`)
 })
